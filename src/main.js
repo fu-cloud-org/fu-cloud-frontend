e8536486aa3e10f334f81d0a05327e4b54be0112
wendaidai <1106518291@qq.com>
2022-11-09 00:22:12 +0800

    add axios config

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
 
 Vue.use(ElementUI);
+Vue.use(Buefy);
 
 Vue.config.productionTip = false
 
