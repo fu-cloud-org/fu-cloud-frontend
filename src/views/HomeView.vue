1bd6ec0f36683adf5ee51227aea8d752001cb0f2
wendaidai <1106518291@qq.com>
2022-11-10 00:04:08 +0800

    billboard finished

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,33 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="box"> {{billboard.content}} </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import {getBillbooard} from "@/api/billboard";
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  data () {
+    return {
+      billboard: {
+        content: ''
+      }
+    }
+  },
+  created() {
+    this.fetchBillboard();
+  },
+  methods: {
+    async fetchBillboard() {
+      getBillbooard().then(value => {
+        const {data} = value;
+        this.billboard = data;
+      })
+    }
+  },
 }
 </script>
