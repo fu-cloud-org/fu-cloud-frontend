7edc2ee03376b903e28e12eff53e0469256cbdfa
wendaidai <1106518291@qq.com>
2022-11-12 21:00:47 +0800

    index post list

--- src/main.js
+++ src/main.js
@@ -7,7 +7,23 @@ import 'element-ui/lib/theme-chalk/index.css';
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 import '@/assets/css/app.css'
+import format from 'date-fns/format'
 import '@/permission'
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+import 'dayjs/locale/zh-cn'
+const dayjs = require('dayjs');
+
+dayjs.extend(relativeTime)
+
+dayjs.locale('zh-cn') // use locale globally
+dayjs().locale('zh-cn').format() // use locale in a specific instance
+
+Vue.prototype.dayjs = dayjs;
+
+Vue.filter('date', (date) => {
+  return format(new Date(date), 'yyyy-MM-dd')
+})
 
 Vue.use(ElementUI);
 Vue.use(Buefy);
