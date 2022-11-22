b91d898473817169030b25437540e8f8af325416
wendaidai <1106518291@qq.com>
2022-11-22 22:45:33 +0800

    fix

--- src/views/authentic/Authentic.vue
+++ src/views/authentic/Authentic.vue
@@ -174,8 +174,8 @@ export default {
           { required: true, message: "请输入账号", trigger: "blur" },
           {
             min: 2,
-            max: 15,
-            message: "长度在 2 到 15 个字符",
+            max: 20,
+            message: "长度在 2 到 20 个字符",
             trigger: "blur",
           },
         ],
