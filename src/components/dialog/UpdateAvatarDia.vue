b16f6a56896d7e93fbc050856df09e6a1826f66f
wendaidai <1106518291@qq.com>
2022-11-25 16:46:16 +0800

    fine-tuning

--- src/components/dialog/UpdateAvatarDia.vue
+++ src/components/dialog/UpdateAvatarDia.vue
@@ -1,30 +1,37 @@
 <template>
   <div>
     <el-dialog
+      custom-class="dialogClass"
       title="修改头像"
       :visible.sync="visible"
       width="40%"
       :before-close="handleClose">
-      <div>
-        <el-upload
-            ref="upload"
-            class="upload-demo"
-            drag
-            :action="upload()"
-            :on-remove="handleRemove"
-            :before-upload="handleBeforeUpload"
-            :on-success="handleSuccess"
-            :auto-upload="true">
-          <i class="el-icon-upload"></i>
-          <div class="el-upload__text">Drop file here or <em>click to upload</em></div>
-          <div class="el-upload__tip" slot="tip">jpg/png files with a size less than 2MB</div>
-        </el-upload>
-        <span slot="footer" class="dialog-footer">
-          <el-button @click="handleClose">取 消</el-button>
-          <el-button type="primary" @click="update" v-show="newPath !== ''">提 交</el-button>
-        </span>
+      <div style="display: flex; padding: 20px">
+        <div>
+          <el-upload
+              ref="upload"
+              class="upload-demo"
+              drag
+              :action="upload()"
+              :on-remove="handleRemove"
+              :before-upload="handleBeforeUpload"
+              :on-success="handleSuccess"
+              :auto-upload="true">
+            <i class="el-icon-upload"></i>
+            <div class="el-upload__text">拖动图片到此处或 <em>点击</em> 进行上传</div>
+            <div class="el-upload__tip" slot="tip">仅仅支持 jpg/png 格式，大小不超过2MB</div>
+          </el-upload>
+        </div>
+        <div>
+          <div class="PersonTop_img" style="margin-left: 100px; margin-top: 20px">
+            <img :src="attachImg(nowAvatar)"/>
+          </div>
+        </div>
       </div>
-
+      <span slot="footer" class="dialog-footer">
+          <el-button @click="handleClose">取 消</el-button>
+          <el-button type="primary" @click="update" v-show="newPath !== ''">更 新</el-button>
+        </span>
     </el-dialog>
   </div>
 </template>
@@ -32,11 +39,14 @@
 <script>
 import {mapGetters} from "vuex";
 import {avatarApi, updateAvatar} from "@/api/personal";
+import {attachImg} from "@/utils/attachImg";
 
 export default {
   name: "UpdateAvatarDia",
   data() {
     return {
+      nowAvatar: '',
+      attachImg,
       newPath: '',
       visible: false
     }
@@ -46,6 +56,9 @@ export default {
         'token',
         'user'
     ])
+  },
+  created() {
+    this.nowAvatar = this.user.avatar
   },
   methods: {
     open() {
@@ -76,12 +89,13 @@ export default {
     },
     handleSuccess(res) {
       this.newPath = res.data;
+      this.nowAvatar = this.newPath;
     },
     update() {
       updateAvatar(this.user.username, this.newPath)
           .then(res => {
             this.$message({
-                message: res.data.message,
+                message: '头像更新成功',
                 type: 'success',
             })
             this.handleClose()
@@ -95,5 +109,21 @@ export default {
 </script>
 
 <style scoped>
+.PersonTop_img {
+  width: 150px;
+  height: 150px;
+  /*background-color: lightgray;*/
+  margin-right: 24px;
+  margin-left: 20px;
+  overflow: hidden;
+  border-radius: 50%;
+  display: block;
+  position: relative;
+}
 
+.PersonTop_img img {
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+}
 </style>
